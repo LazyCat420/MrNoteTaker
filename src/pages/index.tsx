83b9e16941781f29fc5c3fc57676b938a3399a30
LazyCat <porktacos@protonmail.com>
2023-06-14 15:55:41 -0700

    added a edit and delete function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 export default function index() {
     const [myList, setMyList] = useState([]);
     const [inputValue, setInputValue] = useState('test');
+    const [editIndex, setEditIndex] = useState(-1);
 
 
     function handleChange(event) {
@@ -29,29 +30,75 @@ export default function index() {
         setMyList(updatedList);
     }
 
+    function editItem(index, text) {
+      const updatedList = [...myList];
+      updatedList[index] = text;
+      setMyList(updatedList);
+      setEditIndex(-1);
+    }
+
     return (
-    <div>
+      <div>
       <div id="myDIV" className="header">
         <h2>My To Do List</h2>
         <form onSubmit={(event) => addItem(event)}>
-          <input type="text" id="myInput" value={inputValue} onChange={handleChange} placeholder="Title..." />
-          <button className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">Add</button>
-          <span className="counter">
-        {myList.length}
-      </span>
+          <input
+            type="text"
+            id="myInput"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Title..."
+          />
+          <button className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            Add
+          </button>
+          <span className="counter">{myList.length}</span>
         </form>
       </div>
       <ul id="myUL">
-        {myList.map((item,index) => (
-            <li key={index}>
+        {myList.map((item, index) => (
+          <li key={index}>
+            {editIndex === index ? (
+              <form
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  editItem(index, event.target.elements[0].value);
+                }}
+              >
+                <input type="text" defaultValue={item} />
+                <button
+                  type="button"
+                  onClick={() => setEditIndex(-1)}
+                  className="bg-gray-500 hover:bg-gray-700 text-black font-semibold py-2 px-4 rounded"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="bg-blue-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
+                >
+                  Save
+                </button>
+              </form>
+            ) : (
+              <>
                 {item}
-                <button onClick={() => removeItem(item)} 
-                className="bg-red-500 hover:bg-red-700 text-black font-semibold py-2 px-4 rounded">
+                <button
+                  onClick={() => setEditIndex(index)}
+                  className="bg-yellow-500 hover:bg-yellow-700 text-black font-semibold py-2 px-4 rounded"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => removeItem(item)}
+                  className="bg-red-500 hover:bg-red-700 text-black font-semibold py-2 px-4 rounded"
+                >
                   Delete
-                  </button>
-            </li>
+                </button>
+              </>
+            )}
+          </li>
         ))}
-
       </ul>
     </div>
   );
