a1cebacac5f65e4d0fc74f66bf78ea412925aed7
Camrule <cameron.regulus@gmail.com>
2023-06-18 11:46:14 +1200

    title

---
+++ src/_codux/boards/pages/index.board.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBoard } from '@wixc3/react-board';
+import Index from '../../../pages';
+
+export default createBoard({
+    name: 'Index',
+    Board: () => <Index />,
+    environmentProps: {
+        canvasBackgroundColor: '#ffffff'
+    }
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,168 +69,169 @@ export default function Index() {
     }
 
     function saveEditItem(index, text) {
-      const updatedList = [...myList];
-      updatedList[index].name = text.name;
-      updatedList[index].date = text.date;
-      setMyList(updatedList);
-      localStorage.setItem('myList', JSON.stringify(updatedList));
-      setEditIndex();
+        const updatedList = [...myList];
+        updatedList[index].name = text.name;
+        updatedList[index].date = text.date;
+        setMyList(updatedList);
+        localStorage.setItem('myList', JSON.stringify(updatedList));
+        setEditIndex();
     }
 
     function addToFav(item) {
-      if (!favList.includes(item)) {
-        const updatedList = [...favList, item];
-        setFavList(updatedList);
-        localStorage.setItem('fav-list', JSON.stringify(updatedList));
-      }
+        if (!favList.includes(item)) {
+            const updatedList = [...favList, item];
+            setFavList(updatedList);
+            localStorage.setItem('fav-list', JSON.stringify(updatedList));
+        }
     }
 
     function addFavToList(item) {
-      if (!myList.includes(item)) {
-        const updatedList = [...myList, item];
-        setMyList(updatedList);
-      }
+        if (!myList.includes(item)) {
+            const updatedList = [...myList, item];
+            setMyList(updatedList);
+        }
     }
 
     function removeFavItem(item) {
-      const updatedList = favList.filter((listItem) => listItem.name !== item.name);
-      setFavList(updatedList);
-      localStorage.setItem('fav-list', JSON.stringify(updatedList));
-  }
+        const updatedList = favList.filter((listItem) => listItem.name !== item.name);
+        setFavList(updatedList);
+        localStorage.setItem('fav-list', JSON.stringify(updatedList));
+    }
 
     function setToDone(item) {
-      console.log(item);
-      if (!doneList.includes(item)) {
-        const doneItem = { name: item.name, date: item.date, finishDate: new Date().toLocaleString() };
-        const updatedList = [...doneList, doneItem ];
-        setDoneList(updatedList);
-        localStorage.setItem('done-list', JSON.stringify(updatedList));
-      }
-      removeItem(item);
+        console.log(item);
+        if (!doneList.includes(item)) {
+            const doneItem = { name: item.name, date: item.date, finishDate: new Date().toLocaleString() };
+            const updatedList = [...doneList, doneItem];
+            setDoneList(updatedList);
+            localStorage.setItem('done-list', JSON.stringify(updatedList));
+        }
+        removeItem(item);
     }
 
     function removeDoneItem(item) {
-      const updatedList = doneList.filter((listItem) => listItem.name !== item.name);
-      setDoneList(updatedList);
-      localStorage.setItem('done-list', JSON.stringify(updatedList));
-  }
+        const updatedList = doneList.filter((listItem) => listItem.name !== item.name);
+        setDoneList(updatedList);
+        localStorage.setItem('done-list', JSON.stringify(updatedList));
+    }
 
     return (
-      <div>
-      <div id="myDIV" className="header">
-        <form onSubmit={(event) => addItem(event)}>
-          <input
-            type="text"
-            id="myInput"
-            value={inputValue}
-            onChange={handleChange}
-            placeholder="Title..."
-          />
-          <button className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
-            Add
-          </button>
-        </form>
-        <h2>My To Do List ({myList.length})</h2>
-      </div>
-      <ul>
-        {myList.map((item, index) => (
-          <li key={index}>
-            {editIndex === index ? (
-              <form
-                onSubmit={(event) => {
-                  event.preventDefault();
-                  saveEditItem(index, event.target.elements[0].value);
-                }}
-              >
-                <input type="text" defaultValue={item.name} />
-                <button
-                  type="button"
-                  onClick={() => setEditIndex()}
-                  className="bg-gray-500 hover:bg-gray-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  Cancel
-                </button>
-                <button
-                  type="submit"
-                  className="bg-blue-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  Save
-                </button>
-              </form>
-            ) : (
-              <>
-                <span className="name">{item.name}</span>
-                <span>{item.date}</span>
-                <button
-                  onClick={() => setEditIndex(index)}
-                  className="bg-yellow-500 hover:bg-yellow-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => removeItem(item)}
-                  className="bg-red-500 hover:bg-red-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  Delete
-                </button>
-                <button
-                  onClick={() => addToFav(item)}
-                  className="bg-green-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  fav
-                </button>
-                <button
-                  onClick={() => setToDone(item)}
-                  className="bg-gray-500 hover:bg-gray-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  Done
-                </button>
-              </>
-            )}
-          </li>
-        ))}
-      </ul>
-      <h2>My Fav List ({favList.length})</h2>
-      <ul>
-        {favList.map((item, index) => (
-          <li key={index}>
-                {item.name}
-                <button
-                  onClick={() => removeFavItem(item)}
-                  className="bg-blue-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  Delete
-                </button>
-                <button
-                  onClick={() => addFavToList(item)}
-                  className="bg-green-500 hover:bg-green-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  add to list
-                </button>
-          </li>
-        ))}
-      </ul>
-      <h2>Done List ({doneList.length})</h2>
-      <ul>
-        {doneList.map((item, index) => (
-          <li key={index}>
-               <span className="name">{item.name}</span>
-                <span>{item.finishDate}</span>
-                <button
-                  onClick={() => removeDoneItem(item)}
-                  className="bg-blue-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  Delete
-                </button>
-                <button
-                  onClick={() => addToFav(item)}
-                  className="bg-green-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
-                >
-                  fav
-                </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+        <div>
+            <div>
+                <h1>Smart List</h1>
+            </div>
+            <div id="myDIV" className="header">
+                <form onSubmit={(event) => addItem(event)}>
+                    <input
+                        type="text"
+                        id="myInput"
+                        value={inputValue}
+                        onChange={handleChange}
+                        placeholder="Title..."
+                    />
+                    <button className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+                        Add
+                    </button>
+                </form>
+                <h2>My To Do Listsssss{myList.length})</h2></div>
+            <ul>
+                {myList.map((item, index) => (
+                    <li key={index}>
+                        {editIndex === index ? (
+                            <form
+                                onSubmit={(event) => {
+                                    event.preventDefault();
+                                    saveEditItem(index, event.target.elements[0].value);
+                                }}
+                            >
+                                <input type="text" defaultValue={item.name} />
+                                <button
+                                    type="button"
+                                    onClick={() => setEditIndex()}
+                                    className="bg-gray-500 hover:bg-gray-700 text-black font-semibold py-2 px-4 rounded"
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="bg-blue-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
+                                >
+                                    Save
+                                </button>
+                            </form>
+                        ) : (
+                            <>
+                                <span className="name">{item.name}</span>
+                                <span>{item.date}</span>
+                                <button
+                                    onClick={() => setEditIndex(index)}
+                                    className="bg-yellow-500 hover:bg-yellow-700 text-black font-semibold py-2 px-4 rounded"
+                                >
+                                    Edit
+                                </button>
+                                <button
+                                    onClick={() => removeItem(item)}
+                                    className="bg-red-500 hover:bg-red-700 text-black font-semibold py-2 px-4 rounded"
+                                >
+                                    Delete
+                                </button>
+                                <button
+                                    onClick={() => addToFav(item)}
+                                    className="bg-green-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
+                                >
+                                    fav
+                                </button>
+                                <button
+                                    onClick={() => setToDone(item)}
+                                    className="bg-gray-500 hover:bg-gray-700 text-black font-semibold py-2 px-4 rounded"
+                                >
+                                    Done
+                                </button>
+                            </>
+                        )}
+                    </li>
+                ))}
+            </ul>
+            <h2>My Fav List ({favList.length})</h2>
+            <ul>
+                {favList.map((item, index) => (
+                    <li key={index}>
+                        {item.name}
+                        <button
+                            onClick={() => removeFavItem(item)}
+                            className="bg-blue-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
+                        >
+                            Delete
+                        </button>
+                        <button
+                            onClick={() => addFavToList(item)}
+                            className="bg-green-500 hover:bg-green-700 text-black font-semibold py-2 px-4 rounded"
+                        >
+                            add to list
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <h2>Done List ({doneList.length})</h2>
+            <ul>
+                {doneList.map((item, index) => (
+                    <li key={index}>
+                        <span className="name">{item.name}</span>
+                        <span>{item.finishDate}</span>
+                        <button
+                            onClick={() => removeDoneItem(item)}
+                            className="bg-blue-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
+                        >
+                            Delete
+                        </button>
+                        <button
+                            onClick={() => addToFav(item)}
+                            className="bg-green-500 hover:bg-blue-700 text-black font-semibold py-2 px-4 rounded"
+                        >
+                            fav
+                        </button>
+                    </li>
+                ))}
+            </ul></div>
+    );
 }
